bbac4da65e86f22388747f81491ffec6927eb44a
IreneChernobuk <irinka_buc@mail.ru>
2023-03-15 22:23:40 +0300

    homework lesson 15. properties and log

--- src/test/java/drivermanager/FirefoxDriverManager.java
+++ src/test/java/drivermanager/FirefoxDriverManager.java
@@ -1,21 +1,17 @@
 package drivermanager;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
-
-import java.util.Arrays;
 
 public class FirefoxDriverManager extends DriverManager {
 
     @Override
     public void createDriver() {
         WebDriverManager.firefoxdriver().setup();
-        FirefoxProfile firefoxProfile = new FirefoxProfile();
-        //help
-        firefoxProfile.setPreference("--disable-notifications", false);
-        firefoxProfile.setPreference("excludeSwitches", String.valueOf(Arrays.asList("disable-popup-blocking")));
-        driver = new FirefoxDriver(firefoxProfile);
+        FirefoxOptions options = new FirefoxOptions();
+        options.setProfile(new FirefoxProfile());
+        driver = new FirefoxDriver(options);
     }
 }
--- src/test/java/tests/CreateAccountTest.java
+++ src/test/java/tests/CreateAccountTest.java
@@ -2,22 +2,33 @@ package tests;
 
 import data.PrepareNewAccountData;
 import modals.NewAccountModal;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.AccountsPage;
 import pages.LoginPage;
 
 public class CreateAccountTest extends BaseTest {
+    private static final Logger LOGGER = LogManager.getLogger(CreateAccountTest.class.getName());
+
     @Test
     public void createAccountTest() {
         LoginPage loginPage = new LoginPage(driver);
+        LOGGER.info(String.format("Page %s initialized", LoginPage.class.getName()));
         loginPage.loginToSalesForce();
+        LOGGER.info(String.format("Page %s opened", LoginPage.class.getName()));
+        LOGGER.info("Input PASSWORD and USERNAME");
         AccountsPage accountsPage = new AccountsPage(driver);
+        LOGGER.info(String.format("Page %s initialized", AccountsPage.class.getName()));
+        LOGGER.info(String.format("Created %s", NewAccountModal.class.getName()));
         NewAccountModal accountModal = PrepareNewAccountData.getValidData();
         accountsPage
                 .openAccountsPage()
                 .openNewAccountModal()
                 .fillInAccountForm(accountModal);
+        LOGGER.info(String.format("Filled data %s", NewAccountModal.class.getName()));
         Assert.assertEquals(accountModal.getAccountName(), accountsPage.receiveNewAccountName(), "the names don't match");
+        LOGGER.info("Checked the name of the new account is displayed");
     }
 }
